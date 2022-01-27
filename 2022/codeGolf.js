0a1683a62685cbc2353dc0c6a9b66a7d6a3e6c1d
Damien Vesper <ldamienvesper@gmail.com>
2022-01-26 19:59:27 -0600

    spacing

--- 2022/codeGolf.js
+++ 2022/codeGolf.js
@@ -8,4 +8,4 @@ for(let i=0;i<100;i+=2)console.log(i)
  * Problem 1
  * OBJECTIVE: Print the first 30 terms of a Fibonacci sequence starting with 0 and 1.
  */
- let a=[0,1];console.log(0);console.log(1);for(let i=2;i<28;i++){a[i]=a[i-1]+a[i-2];console.log(a[i])}
+let a=[0,1];console.log(0);console.log(1);for(let i=2;i<28;i++){a[i]=a[i-1]+a[i-2];console.log(a[i])}
